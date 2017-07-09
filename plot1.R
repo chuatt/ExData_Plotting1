8616532640f093a6513f87826f75571e380cf450
chuatt <dongda_2@yahoo.com>
2017-07-09 15:15:11 +0800

    Delete plot1.R

--- plot1.R
+++
@@ -1,8 +0,0 @@
-# Open device
-png(filename = "plot1.png", width = 480, height = 480, units = "px")
-
-# Create plot1
-with(df, hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col="red", ylim=c(0,1300)))
-
-# Device off
-dev.off()
