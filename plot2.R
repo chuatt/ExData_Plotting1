de6afa8ed0a31ec4f44013dc01fefdfff1362a10
chuatt <dongda_2@yahoo.com>
2017-07-09 15:27:35 +0800

    Delete plot2.R

--- plot2.R
+++
@@ -1,8 +0,0 @@
-# Open device
-png(filename = "plot2.png", width = 480, height = 480, units = "px")
-
-# Create plot2
-with(df, plot(df$DateTime, df$Global_active_power, xlab = " ", ylab = "Global Active Power (kilowatts)", type = "l"))
-
-# Device off
-dev.off()
